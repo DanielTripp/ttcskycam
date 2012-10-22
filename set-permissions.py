65e3875adf8b5ac430d990ece65311ba7502b243
dt <dt@localhost>
2012-10-22 05:53:38 +0000

    Now omitting .idea directory.

--- set-permissions.py
+++ set-permissions.py
@@ -2,9 +2,12 @@
 
 import os, os.path, stat
 
+DIRS_TO_OMIT = ['.svn', '.idea']
+
 for root, dirs, files in os.walk('.'):
-	if '.svn' in dirs:
-		dirs.remove('.svn')
+	for dir_to_omit in DIRS_TO_OMIT:
+		if dir_to_omit in dirs:
+			dirs.remove(dir_to_omit)
 	for dir in dirs:
 		os.chmod(os.path.join(root, dir), stat.S_IRUSR ^ stat.S_IWUSR ^ stat.S_IXUSR ^ stat.S_IROTH ^ stat.S_IXOTH)
 	for file in files:
