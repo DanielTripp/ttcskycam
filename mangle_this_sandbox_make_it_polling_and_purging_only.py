6646e8478fef01e5aeddbb3824d7d92ce0f8c854
dt <dt@localhost>
2013-02-06 06:56:12 +0000

    Converting the mangle_this_sandbox_make_it_polling_and_purging_only script to python and giving it some command-line options and a non-interactive mode (which is implied if any command-line options are present.)

---
+++ mangle_this_sandbox_make_it_polling_and_purging_only.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys, os, os.path, subprocess, shutil
+from misc import *
+
+if __name__ == '__main__':
+
+	if len(sys.argv) == 1:
+		interactive = True
+		write_mofrs = None
+	elif len(sys.argv) == 2:
+		interactive = False
+		arg = sys.argv[1]
+		if not (arg in ('--write-mofrs', '--no-write-mofrs')):
+			raise Exception('illegal argument')
+		write_mofrs = (arg == '--write-mofrs')
+	else:
+		raise Exception()
+
+	def should_proceed():
+		if interactive:
+			print 'Are you sure?'
+			if raw_input() == 'y':
+				print 'Again - are you sure?'
+				if raw_input() == 'y':
+					return True
+			return False
+		else:
+			return True
+
+	if not should_proceed():
+		sys.exit('We have been told not to proceed.')
+	else:
+		if write_mofrs is None:
+			print 'Write MOFRs?'
+			write_mofrs = (raw_input() == 'y')
+		print 'Writing MOFRs: %s' % write_mofrs
+		if write_mofrs:
+			with open('WRITE_MOFRS', 'w') as fout:
+				pass
+		
+		# The big part - deleting most of the sandbox: 
+		for dirpath, dirnames, filenames in os.walk('.'):
+			for dirname in [x for x in dirnames if x != '.psycopg2']:
+				shutil.rmtree(os.path.join(dirpath, dirname))
+			break
+
+
