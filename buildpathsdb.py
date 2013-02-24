bb56b5580b43f6598d1776e5be4119c4bc8375a0
dt <dt@localhost>
2013-02-24 21:03:03 +0000

    In building paths db - adding case of orig/dest being the same square.
    Also, adding the --fill-in option to buildpathsdb.py.

--- buildpathsdb.py
+++ buildpathsdb.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python2.6
 
-import sys, json, os.path, pprint, sqlite3
+import sys, json, os.path, pprint, sqlite3, getopt
 import paths
+from misc import *
 
 if __name__ == '__main__':
 
-	paths.build_db_main()
+	opts, args = getopt.getopt(sys.argv[1:], '', ['fill-in'])
+	assert len(args) == 0
+	paths.build_db_main(bool(get_opt(opts, 'fill-in')))
 
 
