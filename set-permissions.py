3f95ef446f5f2cc34b7cc3504d735a1b3622a7cc
dt <dt@localhost>
2013-03-14 05:26:04 +0000

    Changing this to make things work on new Cirrus hosting box.

--- set-permissions.py
+++ set-permissions.py
@@ -11,8 +11,5 @@ for root, dirs, files in os.walk('.'):
 	for dir in dirs:
 		os.chmod(os.path.join(root, dir), stat.S_IRUSR ^ stat.S_IWUSR ^ stat.S_IXUSR ^ stat.S_IROTH ^ stat.S_IXOTH)
 	for file in files:
-		if file.endswith('.py') or file.endswith('.pyc'):
-			os.chmod(os.path.join(root, file), stat.S_IRUSR ^ stat.S_IWUSR ^ stat.S_IXUSR)
-		else:
-			os.chmod(os.path.join(root, file), stat.S_IRUSR ^ stat.S_IWUSR ^ stat.S_IXUSR ^ stat.S_IROTH ^ stat.S_IXOTH)
+		os.chmod(os.path.join(root, file), stat.S_IRUSR ^ stat.S_IWUSR ^ stat.S_IXUSR ^ stat.S_IROTH ^ stat.S_IXOTH)
 
