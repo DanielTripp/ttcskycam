b808c9618d5974b8bae55abf108a3546a83bbf3e
dt <dt@localhost>
2013-03-15 03:17:33 +0000

    Adding command-line argument to purgedb.py - 'number of days'.
    Getting rid of the full vacuum.  As far as I know the only benefit of that is to return disk space to the operating system,
    and now we're off theorem.ca (where my space was tight) that's not necessary.
    
    Also adding an 'analyze' to the vacuum in the hope that it will speed up all the queries.  I have no evidence for this yet.

--- purgedb.py
+++ purgedb.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python2.6
 
+import sys, re
 import db
 
 if __name__ == '__main__':
 
-	db.purge()
+	days_arg = sys.argv[1]
+	mo = re.match(r'(\d+)d', days_arg)
+	if not mo:
+		raise Exception('Need a number-of-days value as argument eg. 5d')
+	num_days = int(mo.group(1))
+	db.purge(num_days)
 
