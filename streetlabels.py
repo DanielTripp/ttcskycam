1216cad568fbd9e9f630e87d1a3591d272886eb9
dt <dt@localhost>
2013-03-21 05:00:13 +0000

    Preliminary work on pre-building images, instead of creating them inside the CGI call.

--- streetlabels.py
+++ streetlabels.py
@@ -52,6 +52,7 @@ def get_labels(froute_, zoom_, box_sw_, box_ne_):
 		return start_latlng.is_within_box(box_sw_, box_ne_) or end_latlng.is_within_box(box_sw_, box_ne_)
 	return [label for label in get_labels_for_zoom(froute_, zoom_) if is_within_box(label)]
 
+# return eg. [{'text': 'College St', 'latlng': geom.LatLng(43.0,-79.0), 'end_latlng': geom.LatLng(43.1,-79.1), 'rotation': 10}, ...]
 @mc.decorate
 def get_labels_for_zoom(froute_, zoom_):
 	assert isinstance(zoom_, int)
@@ -84,6 +85,24 @@ def get_labels_for_zoom(froute_, zoom_):
 			start_mofr += mofrstep
 	return r
 
+def get_streetlabel_svg(text_, rotation_, zoom_):
+	fontsize = {13:3.5, 14:4, 15:4, 16:4.5, 17:5, 18:5, 19:5, 20:5, 21:5}[zoom_]
+	# In SVG the y location of text seems to be the baseline i.e. bottom of upper-case letters.  So if we set that baseline 
+	# to be the vertical middle of the graphic, and also use that vertical middle to coincide with the middle of the 
+	# google maps polyline (which we do) then to get the text to sit in the middle of the polyline, we'll have to shift the 
+	# text down.  I don't know why shifting it down by 1/3 of the font size does what we want, but it does. 
+	textshift = fontsize/3.0 
+	# SVG transforms are evaluated right to left.  So below we're translating, then rotating. 
+	svgstr = '''<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 100 100" version="1.1">
+<g transform="rotate(%(rotation)d 50 50) translate(0 %(textshift)f)" >
+<text x="49.7" y="49.7" fill="rgb(255,255,255)" font-family="sans-serif" font-size="%(fontsize)f">%(text)s</text>
+<text x="50" y="50" fill="rgb(80,50,20)" font-family="sans-serif" font-size="%(fontsize)f">%(text)s</text>
+</g>
+</svg>''' % {'text': text_, 'rotation': rotation_, 'fontsize': fontsize, 'textshift': textshift}
+	return svgstr
+
+def get_streetlabel_png(text_, rotation_, zoom_):
+	return svg_to_png(get_streetlabel_svg(text_, rotation_, zoom_))
 
 if __name__ == '__main__':
 
