0f8da90249d599430dc67b2dcdab2e17ceefd1ee
dt <dt@localhost>
2013-05-01 01:58:54 +0000

    The paths building process is now parallelized via python's 'multiprocessing' module.
    On my desktop machine at home, this used to take about 5 days.  Not sure how long it takes now.

--- buildpathsdb.py
+++ buildpathsdb.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
 
 	opts, args = getopt.getopt(sys.argv[1:], '', ['fill-in'])
 	assert len(args) == 0
-	paths.build_db_main(bool(get_opt(opts, 'fill-in')))
+	paths.build_db(bool(get_opt(opts, 'fill-in')))
 
 
