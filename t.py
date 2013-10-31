80bfa50c60badfd9045a961ce204770b4f7e84c7
dt <dt@localhost>
2013-10-31 12:30:32 +0000

    Test commit.

--- t.py
+++ t.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python2.6
 
 import sys, os, urlparse, json, pprint, time, pickle, xml.dom, xml.dom.minidom, datetime, time, getopt
+from itertools import *
 from misc import *
 from backport_OrderedDict import *
-import traffic, db, vinfo, routes, geom, mc, tracks, util, predictions, paths, c, reports, streetlabels, snaptogrid
+import routes, traffic, db, vinfo, geom, mc, tracks, util, predictions, paths, c, reports, streetlabels, snaptogrid
+#from routes import *
 
 if __name__ == '__main__':
 
+	l = range(5)
+	for e in l:
+		print e
+		if e == 3:
+			l.insert(-1, 'x')
 
-	c = snaptogrid.SnapToGridCache([[geom.LatLng(40.0, 80.0), geom.LatLng(40.01, 80.01)]])
-
-	print c.snap(geom.LatLng(40.1, 80.1), None)
-
+	print l
 
