0112cf7480e31e943728a1668e68f50d7c139f95
dt <dt@localhost>
2013-11-28 08:10:31 +0000

    Optimizing creation of streets snapgraph.   Making one for all of Toronto's streets now takes about 6 minutes on the Cirrus VPS Plan IV.  I don't know how long it took before this commit, because I never did it all.  Somewhere around 10 times as long, I think.
    The idea with the change in snapgraph.py (to only yield an addr pair if the
    second polylineidx is greater than the first) is because by default this code
    yields almost every pair twice - once in one order (addr1, addr2) and once in
    the opposite order (addr2, addr1).  So this change makes it yield just one of
    those.
    
    The reason it's /almost/ twice is because of what happens when we're yielding
    point/point pairs and we're at the end of a polyline.  We yield a little too
    much here because the structure we're using to determine which points are in
    our gridsquares of interest is gridsquare_to_linesegaddrs - which, true to the
    name, identifies linesegs, not points.   I don't feel like explaning this any
    more right now.  What can I say.  I think it works.

--- streets.py
+++ streets.py
@@ -10,6 +10,44 @@ import shapefile
 
 @picklestore.decorate
 def get_polylines():
+	r = get_polylines_from_shapefile()
+	simplify_polylines(r)
+	return r
+
+# Modifies argument. 
+def simplify_polylines(polylines_):
+	epsilon = 5
+	for polyline in polylines_:
+		polyline[:] = get_simplified_polyline(polyline, epsilon)
+
+# Thanks to http://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm 
+def get_simplified_polyline(pline_, epsilon_):
+	if len(pline_) <= 2:
+		return pline_
+	dmax = 0
+	index = 0
+	end = len(pline_)
+	if pline_[0].is_close(pline_[-1]):
+		# so pline_ is a loop, and the dist_to_lineseg() call below will fail if we try it.  
+		# let's go straight to the recurse.  
+		dmax = epsilon_*2
+	else:
+		for i in range(1, len(pline_)-1):
+			d = pline_[i].dist_to_lineseg(geom.LineSeg(pline_[0], pline_[-1]))
+			if d > dmax:
+				index = i
+				dmax = d
+
+	# If max distance is greater than epsilon, recursively simplify
+	if dmax > epsilon_:
+		recResults1 = get_simplified_polyline(pline_[0:index+1], epsilon_)
+		recResults2 = get_simplified_polyline(pline_[index+1:], epsilon_)
+		return recResults1[:-1] + recResults2
+	else:
+		return [pline_[0], pline_[-1]]
+
+@picklestore.decorate
+def get_polylines_from_shapefile():
 	street_fcode_descs = ['Access Road', 'Busway', 'Collector', 'Collector Ramp', 'Expressway', 'Expressway Ramp', 'Local', 'Major Arterial', 'Major Arterial Ramp', 'Minor Arterial', 'Minor Arterial Ramp', 'Pending', 'Other', 'Other Ramp']
 	sf = shapefile.Reader('toronto_street_map/centreline_wgs84/CENTRELINE_WGS84')
 	fields = [x for x in sf.fields if x[0] != 'DeletionFlag']
