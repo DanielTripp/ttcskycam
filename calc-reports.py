00b6f2839ceb82d8117b800ff493d1c0cd2363cb
dt <dt@localhost>
2015-03-04 01:15:23 +0000

    calc-reports.py now shows locations report sorted by vid by default.

--- calc-reports.py
+++ calc-reports.py
@@ -12,9 +12,61 @@ from backport_OrderedDict import *
 import traffic, db, vinfo, routes, geom, mc, tracks, util, predictions, system, c, reports, streetlabels, snapgraph, streets
 import numpy as np
 
+def expand(str_):
+	if str_.startswith('{') and str_.endswith('}'):
+		if ',' in str_:
+			return str_[1:-1].split(',')
+		elif '..' in str_:
+			parts = str_[1:-1].split('..')
+			if len(parts) not in (2, 3):
+				raise Exception()
+			if len(parts[0]) != len(parts[1]):
+				raise Exception()
+			if ':' in parts[0]:
+				assert (':' in parts[1]) and all(re.match(r'\d\d:\d\d', x) for x in parts[:2])
+				def toint(x__):
+					splits = x__.split(':')
+					return int(splits[0])*60 + int(splits[1])
+				def tostr(x__):
+					return '%02d:%02d' % (x__/60, x__%60)
+			else:
+				def toint(x__):
+					return int(x__)
+				length = len(parts[0])
+				def tostr(x__):
+					return ('%0'+str(length)+'d') % x
+			start = toint(parts[0]); stop = toint(parts[1])
+			inc = int(parts[2]) if len(parts) == 3 else 1
+			return [tostr(x) for x in range(start, stop+1, inc)]
+	else:
+		return [str_]
+
+def get_locations_by_vid(report_):
+	r = defaultdict(list)
+	for timeslice in report_:
+		for vi in timeslice[1:]:
+			r[vi.vehicle_id].append(vi)
+	return dict(r)
+
+def print_traffic_report(report_):
+	pprint.pprint(report_)
+
+def print_locations_report(report_, locsbyvid_):
+	if locsbyvid_:
+		for vid, vis in get_locations_by_vid(report_).iteritems():
+			print 'vid=%s' % vid
+			for vi in vis:
+				print '  %s' % vi
+	else:
+		for timeslice in report_:
+			timestr = timeslice[0]
+			print timestr
+			for vi in timeslice[1:]:
+				print '  %s' % vi
+
 if __name__ == '__main__':
 
-	opts, args = getopt.getopt(sys.argv[1:], '', ['print', 'datazooms=', 'log', 'norestartmc', 'locsbyvid'])
+	opts, args = getopt.getopt(sys.argv[1:], '', ['print', 'datazooms=', 'log', 'norestartmc', 'nolocsbyvid'])
 	if len(args) != 4:
 		printerr('Usage:')
 		printerr('Mandatory args: reporttype(s), froute(s), direction(s), and date/time(s).')
@@ -24,14 +76,14 @@ if __name__ == '__main__':
 	doprint = get_opt(opts, 'print')
 	datazooms_arg = get_opt(opts, 'datazooms')
 	if not datazooms_arg:
-		datazooms = [0]
+		datazooms = [c.MAX_DATAZOOM]
 	elif datazooms_arg == 'all':
 		datazooms = c.VALID_DATAZOOMS[:]
 	else:
 		datazooms = [int(e) for e in datazooms_arg.split(',')]
 	dolog = get_opt(opts, 'log')
 	norestartmc = get_opt(opts, 'norestartmc')
-	locsbyvid = get_opt(opts, 'locsbyvid')
+	nolocsbyvid = get_opt(opts, 'nolocsbyvid')
 
 	reporttypes, froutes, directions, times = args
 
@@ -54,42 +106,6 @@ if __name__ == '__main__':
 	if not mo:
 		sys.exit('Invalid times arg.')
 	yeararg, montharg, dayarg, timearg = [mo.group(i) for i in range(1, 5)]
-
-	def expand(str_):
-		if str_.startswith('{') and str_.endswith('}'):
-			if ',' in str_:
-				return str_[1:-1].split(',')
-			elif '..' in str_:
-				parts = str_[1:-1].split('..')
-				if len(parts) not in (2, 3):
-					raise Exception()
-				if len(parts[0]) != len(parts[1]):
-					raise Exception()
-				if ':' in parts[0]:
-					assert (':' in parts[1]) and all(re.match(r'\d\d:\d\d', x) for x in parts[:2])
-					def toint(x__):
-						splits = x__.split(':')
-						return int(splits[0])*60 + int(splits[1])
-					def tostr(x__):
-						return '%02d:%02d' % (x__/60, x__%60)
-				else:
-					def toint(x__):
-						return int(x__)
-					length = len(parts[0])
-					def tostr(x__):
-						return ('%0'+str(length)+'d') % x
-				start = toint(parts[0]); stop = toint(parts[1])
-				inc = int(parts[2]) if len(parts) == 3 else 1
-				return [tostr(x) for x in range(start, stop+1, inc)]
-		else:
-			return [str_]
-
-	def get_locations_by_vid(report_):
-		r = defaultdict(list)
-		for timeslice in report_:
-			for vi in timeslice[1:]:
-				r[vi.vehicle_id].append(vi)
-		return dict(r)
 
 	stdout_and_stderr_both_ttys = os.isatty(sys.stdout.fileno()) and os.isatty(sys.stderr.fileno())
 	stderr_is_a_tty = os.isatty(sys.stderr.fileno())
@@ -126,12 +142,13 @@ if __name__ == '__main__':
 							report = traffic.get_traffics_impl(froute, direction, datazoom, time_em, log_=dolog)
 						elif reporttype == 'l':
 							report = traffic.get_recent_vehicle_locations_impl(froute, direction, datazoom, time_em, log_=dolog)
-							if locsbyvid:
-								report = get_locations_by_vid(report)
 						else:
 							raise Exception()
 						if doprint:
-							pprint.pprint(report)
+							if reporttype == 't':
+								print_traffic_report(report)
+							else:
+								print_locations_report(report, not nolocsbyvid)
 		if profile:
 			if i == 0:
 				profiler = cProfile.Profile() # tdr 
@@ -139,6 +156,5 @@ if __name__ == '__main__':
 		cpu_prof_exit_early_maybe()
 	if profile:
 		dump_profiler_to_svg_file(profiler, None)
-	printerr('All reports took %.3f seconds.' % (time.time() - total_t0))
 
 
