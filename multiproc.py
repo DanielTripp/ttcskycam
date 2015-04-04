ac6779b2e2d4695c99677d3ba106a8eab3123f63
dt <dt@localhost>
2015-04-04 23:03:32 +0000

    Introducing the ShardPool class.  Using it for reports generation, so that patch-caches have a chance of being useful.  Before this commit, reports for any given route would generally be generated on a different work process every minute, so the patch-caches (which exist only in the worker process) didn't have a chance.

--- multiproc.py
+++ multiproc.py
@@ -4,6 +4,7 @@ import sys, os, os.path, time, math, datetime, calendar, bisect, tempfile, subpr
 from collections import Sequence, MutableSequence, defaultdict, MutableSet
 from itertools import *
 import db, mc
+from misc import *
 
 def initializer():
 	db.forget_connection()
@@ -20,6 +21,41 @@ def run(n_, func_, argses_):
 	pool.close()
 	pool.join()
 	return returnvals
+
+# Thanks to http://stackoverflow.com/a/16618842/321556 
+def include_entire_traceback(real_func_):
+	@functools.wraps(real_func_)
+	def decorating_function(*args, **kwds):
+		try:
+			return real_func_(*args, **kwds)
+		except:
+			raise Exception("".join(traceback.format_exception(*sys.exc_info())))
+	
+	return decorating_function
+
+class ShardPool(object):
+
+	def __init__(self, shardfunc_, processes=None):
+		self.shardfunc = shardfunc_
+		self.num_processes = (processes if processes is not None else multiprocessing.cpu_count())
+		self.pools = tuple(multiprocessing.Pool(1, initializer=initializer) for i in xrange(self.num_processes))
+
+	def apply_async(self, func_, args_):
+		pool_idx = self.shardfunc(func_, args_) % self.num_processes
+		pool = self.pools[pool_idx]
+		return pool.apply_async(func_, args_)
+
+	def map(self, func_, argses_):
+		async_results = []
+		exc = None
+		for args in argses_:
+			try:
+				async_results.append(self.apply_async(func_, args))
+			except e:
+				exc = e
+		if exc is not None:
+			raise exc
+		return [async_result.get() for async_result in async_results]
 
 if __name__ == '__main__':
 
