f4106769dc0453a05a7c086d79031023dc1c7972
danieltripp <danieltripp@localhost>
2015-04-28 22:55:48 -0400

    Adding GPL license headers and COPYING file.

--- reports.py
+++ reports.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+'''
+This file is part of ttcskycam.
+
+ttcskycam is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ttcskycam is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ttcskycam.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 from collections import *
 import sys, os, os.path, json, argparse, traceback, time, pprint
 import vinfo, db, routes, geom, mc, yards, traffic, c, util, multiproc, streets, tracks
