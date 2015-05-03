95a8ff9710ea1f7988952af33699b94b1da5dd17
danieltripp <danieltripp@localhost>
2015-05-03 00:57:48 -0400

    Moving streetcar tracks data files into 'tracks' directory.

--- tracks.py
+++ tracks.py
@@ -30,8 +30,8 @@ def get_snapgraph():
 
 def get_polylines_from_files():
 	r = []
-	for track_filename in sorted(f for f in os.listdir('.') if f.startswith('tracks_') and f.endswith('.json')):
-		r.append(get_polyline_from_file(track_filename))
+	for track_filename in sorted(f for f in os.listdir('tracks') if f.endswith('.json')):
+		r.append(get_polyline_from_file(os.path.join('tracks', track_filename)))
 	return r
 
 def get_polyline_from_file(filename_):
@@ -45,7 +45,7 @@ def get_polyline_from_file(filename_):
 				r.append(geom.LatLng(raw_latlng[0], raw_latlng[1]))
 			return r
 	except:
-		printerr('error with %s' % track_filename)
+		printerr('error with %s' % filename_)
 		raise
 	
 if __name__ == '__main__':
